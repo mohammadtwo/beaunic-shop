57bb5f3157a80783f554238d6314ac8951d151ad
erfansarlak19-hue <erfansarlak19@gmail.com>
2026-02-09 15:21:59 +0330

    feat(home): add colleagues section

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,3 +1,7 @@
+import { BestSellingProducts } from "../product/bestSellingProducts";
+import { LatestCosmeticsProducts } from "../product/latestCosmeticsProducts";
+import Colleagues from "./Colleagues/Colleagues";
+
 import { Slider } from "./slider";
 
 
@@ -5,6 +9,9 @@ export function Home() {
   return (
     <div className=" w-full flex flex-col">
       <Slider/>
+            <BestSellingProducts />
+            <LatestCosmeticsProducts/>
+            <Colleagues/>
     </div>
   );
 }
